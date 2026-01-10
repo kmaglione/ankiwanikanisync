92671de54aa5103266d3805e316dd612cf4acaf3
Kris Maglione <maglione.k@gmail.com>
2026-01-09 17:23:56 -0800

    Extract WdioCheckElementMethodOptions from toMatchElementSnapshot parameters rather than recreating it

--- wdio-tests/util.ts
+++ wdio-tests/util.ts
@@ -1,5 +1,3 @@
-import type { CheckElementMethodOptions, CheckFullPageMethodOptions, CheckScreenMethodOptions, SaveElementMethodOptions, SaveFullPageMethodOptions, SaveScreenMethodOptions } from "@wdio/image-comparison-core";
-
 declare module "webdriverio" {
     interface ChainablePromiseArray {
         then<U>(
@@ -9,29 +7,7 @@ declare module "webdriverio" {
     }
 }
 
-export interface WdioIcsOptions {
-    logName?: string;
-    name?: string;
-}
-export interface WdioIcsCommonOptions {
-    hideElements?: (WebdriverIO.Element | ChainablePromiseElement)[];
-    removeElements?: (WebdriverIO.Element | ChainablePromiseElement)[];
-}
-export interface WdioIcsScrollOptions extends WdioIcsCommonOptions {
-    hideAfterFirstScroll?: (WebdriverIO.Element | ChainablePromiseElement)[];
-}
-export interface WdioSaveScreenMethodOptions extends Omit<SaveScreenMethodOptions, keyof WdioIcsCommonOptions>, WdioIcsCommonOptions {
-}
-export interface WdioSaveElementMethodOptions extends Omit<SaveElementMethodOptions, keyof WdioIcsCommonOptions>, WdioIcsCommonOptions {
-}
-export interface WdioSaveFullPageMethodOptions extends Omit<SaveFullPageMethodOptions, keyof WdioIcsScrollOptions>, WdioIcsScrollOptions {
-}
-export interface WdioCheckScreenMethodOptions extends Omit<CheckScreenMethodOptions, keyof WdioIcsCommonOptions>, WdioIcsCommonOptions {
-}
-export interface WdioCheckElementMethodOptions extends Omit<CheckElementMethodOptions, keyof WdioIcsCommonOptions>, WdioIcsCommonOptions {
-}
-export interface WdioCheckFullPageMethodOptions extends Omit<CheckFullPageMethodOptions, keyof WdioIcsScrollOptions>, WdioIcsScrollOptions {
-}
+export type WdioCheckElementMethodOptions = Parameters<ExpectWebdriverIO.Matchers<void, ChainablePromiseElement>["toMatchElementSnapshot"]>[1];
 
 export async function matchElementSnapshot(element: ChainablePromiseElement, tag: string, expectedResult?: number, options?: WdioCheckElementMethodOptions): Promise<void>;
 export async function matchElementSnapshot(element: ChainablePromiseElement, tag: string, options?: WdioCheckElementMethodOptions): Promise<void>;
